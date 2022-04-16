d60e3ae70e752391bca798897e4a25c345123426
Patrick Patenaude <ppatenaude@hotmail.fr>
2022-04-15 23:35:51 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FormCreerClient = New System.Windows.Forms.MenuStrip()
+        Me.ContratToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ModifierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsulterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RésilierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.QuitterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreerClientToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FournisseurToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ServiceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FactureToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DuJourToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AutreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.TaxesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RapportsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DébitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdminToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NouvelEmployéToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AideToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ÀProposToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IndexeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FormCreerClient.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FormCreerClient
+        '
+        Me.FormCreerClient.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ContratToolStripMenuItem, Me.ClientsToolStripMenuItem, Me.FournisseurToolStripMenuItem, Me.ServiceToolStripMenuItem, Me.FactureToolStripMenuItem, Me.RapportsToolStripMenuItem, Me.AdminToolStripMenuItem, Me.AideToolStripMenuItem})
+        Me.FormCreerClient.Location = New System.Drawing.Point(0, 0)
+        Me.FormCreerClient.Name = "FormCreerClient"
+        Me.FormCreerClient.Size = New System.Drawing.Size(800, 24)
+        Me.FormCreerClient.TabIndex = 2
+        Me.FormCreerClient.Text = "Créer Client"
+        '
+        'ContratToolStripMenuItem
+        '
+        Me.ContratToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.ModifierToolStripMenuItem, Me.ConsulterToolStripMenuItem, Me.RésilierToolStripMenuItem, Me.ToolStripMenuItem1, Me.QuitterToolStripMenuItem})
+        Me.ContratToolStripMenuItem.Name = "ContratToolStripMenuItem"
+        Me.ContratToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.ContratToolStripMenuItem.Text = "Contrat"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "Nouveau"
+        '
+        'ModifierToolStripMenuItem
+        '
+        Me.ModifierToolStripMenuItem.Name = "ModifierToolStripMenuItem"
+        Me.ModifierToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ModifierToolStripMenuItem.Text = "Modifier"
+        '
+        'ConsulterToolStripMenuItem
+        '
+        Me.ConsulterToolStripMenuItem.Name = "ConsulterToolStripMenuItem"
+        Me.ConsulterToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ConsulterToolStripMenuItem.Text = "Consulter"
+        '
+        'RésilierToolStripMenuItem
+        '
+        Me.RésilierToolStripMenuItem.Name = "RésilierToolStripMenuItem"
+        Me.RésilierToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RésilierToolStripMenuItem.Text = "Résilier"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        '
+        'QuitterToolStripMenuItem
+        '
+        Me.QuitterToolStripMenuItem.Name = "QuitterToolStripMenuItem"
+        Me.QuitterToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.QuitterToolStripMenuItem.Text = "Quitter"
+        '
+        'ClientsToolStripMenuItem
+        '
+        Me.ClientsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreerClientToolStripMenuItem})
+        Me.ClientsToolStripMenuItem.Name = "ClientsToolStripMenuItem"
+        Me.ClientsToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.ClientsToolStripMenuItem.Text = "Client"
+        '
+        'CreerClientToolStripMenuItem
+        '
+        Me.CreerClientToolStripMenuItem.Name = "CreerClientToolStripMenuItem"
+        Me.CreerClientToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CreerClientToolStripMenuItem.Text = "Créer Client"
+        '
+        'FournisseurToolStripMenuItem
+        '
+        Me.FournisseurToolStripMenuItem.Name = "FournisseurToolStripMenuItem"
+        Me.FournisseurToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
+        Me.FournisseurToolStripMenuItem.Text = "Fournisseur"
+        '
+        'ServiceToolStripMenuItem
+        '
+        Me.ServiceToolStripMenuItem.Name = "ServiceToolStripMenuItem"
+        Me.ServiceToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.ServiceToolStripMenuItem.Text = "Service"
+        '
+        'FactureToolStripMenuItem
+        '
+        Me.FactureToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DuJourToolStripMenuItem, Me.AutreToolStripMenuItem, Me.ToolStripMenuItem2, Me.TaxesToolStripMenuItem})
+        Me.FactureToolStripMenuItem.Name = "FactureToolStripMenuItem"
+        Me.FactureToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
+        Me.FactureToolStripMenuItem.Text = "Facture"
+        '
+        'DuJourToolStripMenuItem
+        '
+        Me.DuJourToolStripMenuItem.Name = "DuJourToolStripMenuItem"
+        Me.DuJourToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DuJourToolStripMenuItem.Text = "Du jour"
+        '
+        'AutreToolStripMenuItem
+        '
+        Me.AutreToolStripMenuItem.Name = "AutreToolStripMenuItem"
+        Me.AutreToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AutreToolStripMenuItem.Text = "Autre"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(177, 6)
+        '
+        'TaxesToolStripMenuItem
+        '
+        Me.TaxesToolStripMenuItem.Name = "TaxesToolStripMenuItem"
+        Me.TaxesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TaxesToolStripMenuItem.Text = "Taxes"
+        '
+        'RapportsToolStripMenuItem
+        '
+        Me.RapportsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DébitToolStripMenuItem})
+        Me.RapportsToolStripMenuItem.Name = "RapportsToolStripMenuItem"
+        Me.RapportsToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.RapportsToolStripMenuItem.Text = "Rapports"
+        '
+        'DébitToolStripMenuItem
+        '
+        Me.DébitToolStripMenuItem.Name = "DébitToolStripMenuItem"
+        Me.DébitToolStripMenuItem.Size = New System.Drawing.Size(139, 22)
+        Me.DébitToolStripMenuItem.Text = "Transactions"
+        '
+        'AdminToolStripMenuItem
+        '
+        Me.AdminToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NouvelEmployéToolStripMenuItem})
+        Me.AdminToolStripMenuItem.Name = "AdminToolStripMenuItem"
+        Me.AdminToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.AdminToolStripMenuItem.Text = "Admin"
+        '
+        'NouvelEmployéToolStripMenuItem
+        '
+        Me.NouvelEmployéToolStripMenuItem.Name = "NouvelEmployéToolStripMenuItem"
+        Me.NouvelEmployéToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.NouvelEmployéToolStripMenuItem.Text = "Nouvel Employé"
+        '
+        'AideToolStripMenuItem
+        '
+        Me.AideToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ÀProposToolStripMenuItem, Me.IndexeToolStripMenuItem})
+        Me.AideToolStripMenuItem.Name = "AideToolStripMenuItem"
+        Me.AideToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.AideToolStripMenuItem.Text = "Aide"
+        '
+        'ÀProposToolStripMenuItem
+        '
+        Me.ÀProposToolStripMenuItem.Name = "ÀProposToolStripMenuItem"
+        Me.ÀProposToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ÀProposToolStripMenuItem.Text = "À propos"
+        '
+        'IndexeToolStripMenuItem
+        '
+        Me.IndexeToolStripMenuItem.Name = "IndexeToolStripMenuItem"
+        Me.IndexeToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.IndexeToolStripMenuItem.Text = "Indexe"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(129, 180)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(543, 90)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Bienvenue sur ABONNET" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Veuillez choisir l'onglet approprié dans le haut de la p" &
+    "age"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.FormCreerClient)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.FormCreerClient.ResumeLayout(False)
+        Me.FormCreerClient.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FormCreerClient As MenuStrip
+    Friend WithEvents ContratToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ModifierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConsulterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RésilierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents QuitterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreerClientToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FournisseurToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ServiceToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FactureToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DuJourToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AutreToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents TaxesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RapportsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DébitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DébitToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CréditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ArgentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChèqueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents TotalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AdminToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NouvelEmployéToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AideToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ÀProposToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IndexeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Dim FormNouveauContrat As New Form2
+        FormNouveauContrat.Show()
+    End Sub
+
+    Private Sub ModifierToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ModifierToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ConsulterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ConsulterToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub RésilierToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RésilierToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub QuitterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitterToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CreerClientToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreerClientToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub DuJourToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DuJourToolStripMenuItem.Click
+        MessageBox.Show("Les factures du jour sont envoyées à l'imprimante", "Impression", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub AutreToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AutreToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub TaxesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TaxesToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub DébitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DébitToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub NouvelEmployéToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NouvelEmployéToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ÀProposToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ÀProposToolStripMenuItem.Click
+
+        MessageBox.Show("Version 1.0", "Version de l'application", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub IndexeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles IndexeToolStripMenuItem.Click
+        MessageBox.Show("Désoler. Cette fonctionnalité sera intégrée dans la prochaine version", "Projet en développement", MessageBoxButtons.OK)
+    End Sub
+
+
+End Class
